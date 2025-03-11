0047a900c862f90c0781fb6c38569803869fbd13
cnguyen50 <calvinnguyen50@gmail.com>
2025-03-11 09:46:45 -0700

    created thanks.html

--- scripts/review.js
+++ scripts/review.js
@@ -1,6 +1,16 @@
-// function saveHikeDocumentIDAndRedirect() {
+var hikeDocID = localStorage.getItem("hikeDocID");    //visible to all functions on this page
 
-// }
+function getHikeName(id) {
+    db.collection("hikes")
+    .doc(id)
+    .get()
+    .then((thisHike) => {
+        var hikeName = thisHike.data().name;
+        document.getElementById("hikeName").innerHTML = hikeName;
+    });
+}
+
+getHikeName(hikeDocID);
 
 // function writeReview() {
 
