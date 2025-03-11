1ad8f175cad6b51ff914366e24887a35fb0a3d9b
cnguyen50 <calvinnguyen50@gmail.com>
2025-03-11 08:57:19 -0700

    linked profile.html with js file

--- scripts/profile.js
+++ scripts/profile.js
@@ -40,3 +40,20 @@ function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
 }
+
+// function saveUserInfo() {
+//     userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+//     userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
+//     userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
+
+//     currentUser.update({
+//         name: userName,
+//         school: userSchool,
+//         city: userCity
+//     })
+//     .then(() => {
+//         console.log("Document successfully updated!");
+//     })
+
+//     document.getElementById('personalInfoFields').disabled = true;
+// }
