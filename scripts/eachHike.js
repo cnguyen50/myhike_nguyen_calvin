a2b00919840b56cf1cb1e444e843f9f236b125d1
cnguyen50 <calvinnguyen50@gmail.com>
2025-03-11 11:32:13 -0700

    added some styling to center text and stars

--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -69,9 +69,9 @@ function populateReviews() {
 
                 // Populate the star rating based on the rating value
                 
-	              // Initialize an empty string to store the star rating HTML
-								let starRating = "";
-								// This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+	            // Initialize an empty string to store the star rating HTML
+                let starRating = "";
+                // This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
                 for (let i = 0; i < rating; i++) {
                     starRating += '<span class="material-icons">star</span>';
                 }
